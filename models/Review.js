5d2c1d7e3e7c56c8a38b8104e46916e61cd12b50
Gregg <43915943+GreggTri@users.noreply.github.com>
2022-04-27 17:33:45 -0700

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -5,8 +5,10 @@ const ReviewSchema = new mongoose.Schema({
     reviewDate: {type: Date, default: Date.now, required: true},
     product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    userFirstName: {type: String, required: true},
+    userLastName: {type: String, required: true},
     content: {type: String, required: true},
-    stars: {type: String, required: true}
+    rating: {type: Number, required: true}
 }, {timestamps: true} )
 
 
