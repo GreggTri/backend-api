2d1c675b547a95f578a3159e1ecfc95f3ab6e0ef
Gregg <43915943+GreggTri@users.noreply.github.com>
2022-04-27 17:32:23 -0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose')
 
 const OrderSchema = new mongoose.Schema({
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    cart: {type: Object, required: true},
-    address: {type: String, required: true},
-    paymentId: {type: String, required: true},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    orderedProducts: [{
+        _id: false,
+        productId: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
+        productImage: {type: String, required: true},
+        productName: {type: String, required: true},
+        brandName: {type: String, required: true},
+        colorName: {type: String, required: true},
+        price: {type: String, required: true},
+        quantity: {type: Number, required: true},
+        trackingId: {type: String, required: true},
+        status: {type: String, required: true}
+    }],
+    address: {
+        street: {type: String, required: true},
+        city:{type: String, required: true},
+        state: {type: String, required: true},
+        zipcode: {type: String, required: true},
+    },
+    subtotal: {type: String, required: true},
+    tax: {type: String, required: true},
+    purchaseDate: {type: Date, required: true},
 }, {timestamps: true} )
 
 
