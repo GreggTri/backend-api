f46556ddded6562beca626dc0b68c34dfce2532f
Gregg <43915943+GreggTri@users.noreply.github.com>
2022-04-27 17:37:00 -0700

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -67,6 +67,43 @@ router.post('/webhook', async (req, res) => {
   
     if (eventType === 'payment_intent.succeeded') {
       // Funds have been captured
+      try{
+        const products = {}
+
+        for(product in data.cart){
+            let temp = await Product.find(product.productId).select('_id productName productImage seller.brandName colorway.colorName price')
+
+
+            products += [{
+                productId: temp._id,
+                productName: temp.productName,
+                productImage: temp.productImage,
+                brandName: temp.seller.brandName,
+                colorname: temp.colorway.colorName,
+                price: temp.price,
+                quantity: product.quantity,
+                trackingId: "",
+                status: "Order Placed"
+            }]
+        }
+
+        console.log(products)
+        const newOrder = new Order({
+            userId: data.userId,
+            orderedProducts: products,
+            subtotal: calculateOrderAmount(products),
+            tax: data.tax,
+            address: data.address,
+            purchaseDate: Date.now()
+        })
+        await newOrder.save()
+
+        res.status(200).json("ok")
+        } catch(error){
+            console.log(error)
+            res.status(500).json(error)
+        }
+
       // Fulfill any orders, e-mail receipts, etc
       // To cancel the payment after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds)
       console.log('💰 Payment captured!');
@@ -75,5 +112,30 @@ router.post('/webhook', async (req, res) => {
     }
     res.sendStatus(200);
   });
-  
+
+router.get('/get-orders', async (req, res, next) => {
+    try{
+        const orders = Order.find({userId: req.body.userId}).select('_id userId orderedProducts address subtotal tax purchaseDate')
+
+        if(orders != null){
+            console.log(orders)
+            res.status(200).json(orders)
+
+        } else {
+            res.status(404).json("No orders found!")
+        }
+
+    }catch(error){
+        console.log(error)
+
+        res.status(500).json(error)
+    }
+    
+})
+
+router.post('/refund', async (req, res, next) => {
+
+})
+
+
 module.exports = router
