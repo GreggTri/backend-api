f54ffe36ab5b5e5b09d3d320b55d08284f302753
Gregg <43915943+GreggTri@users.noreply.github.com>
2022-04-27 17:47:37 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
+const Product = require("../models/Product")
+const Seller = require("../models/Seller")
 
 //**************************************GET requests*********************************************************
 
-//GET ALL users
 router.get("/", (req, res, next) => {
     User.find()
     .select("_id firstName lastName email address")
@@ -37,18 +38,85 @@ router.get("/", (req, res, next) => {
     })
 })
 
+router.get('/query', async (req, res, next) => {
+    query = req.body.query
+    ///^bar$/i
+    try{
+        brandResults = await Seller.find(
+            {brandName: {$regex: query, $options: 'i'}, isVerified: true}
+            //{isVerified: true}
+        )
+        .select(' brandName brandLogo customerService numOfProducts')
+
+        productResults = await Product.find(
+            {productName: {$regex: query, $options: 'i'}, 'seller.isVerified': true}
+            //{'seller.isVerified': true}
+        )
+        .select(' productName productImage desc price category seller colorway countInStock')
+
+        let resultCount = brandResults.length + productResults.length
+        queryResults = {
+            numOfResults: resultCount,
+            brands: brandResults,
+            products: productResults
+        }
+
+        res.status(200).json(queryResults)
+
+    } catch(error){
+        console.log(error)
+        res.status(500).json(error)
+    }
+})
+
+router.get('/favorites', async (req, res, next) => {
+    idList = req.body
+    try{
+        console.log(idList)
+        const results = await Product.find({_id: { $in: idList}})
+        res.status(200).json(results)
+
+    } catch(error) {
+        res.status(500).json(error)
+    }
+})
+
+router.get('/cart', async (req, res, next) => {
+    idList = req.body
+    try{
+        console.log(idList)
+        //TODO
+        const results = await Product.find({_id: { $in: idList}})
+        res.status(200).json(results)
+
+    } catch(error) {
+        res.status(500).json(error)
+    }
+})
+
 //******************************************POST requests*******************************************************
 
 router.post('/login', async (req, res, next) =>{
+    let userEmail = req.body.email.toLowerCase()
     try{
-        let user = await User.findOne({email: req.body.email}).exec()
-        console.log(user)
+        let user = await User.findOne({email: userEmail}).select('_id firstName lastName email password favorites cart address ')
         if(user == null){
-            return res.status(400).send("Wrong email or password. Please try again")
+            return res.status(400).send("This user does not exist")
         }
 
         if(await bcrypt.compare(req.body.password, user.password)){
-            res.status(200).send('You have Successfully logged in!')
+            await User.findOneAndUpdate({_id: user._id}, {"lastActive": Date.now()}).exec()
+            
+            userWithoutPass = {
+                "_id": user._id,
+                "firstName": user.firstName,
+                "lastName": user.lastName,
+                "email": user.email,
+                "favorites": user.favorites,
+                "cart": user.cart,
+            }
+            res.status(200).json(userWithoutPass)
+
         }else{
             res.status(400).send('Wrong email or password. Please try again')
         }
@@ -58,22 +126,16 @@ router.post('/login', async (req, res, next) =>{
 })
 
 router.post("/", async (req, res, next) =>{
-    console.log(req.body)
     try{
-        if(req.body.password !== req.body.confirmPassword){
-            
-            return res.status(400).send('Passwords do not match! Please try again')
-        }
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         console.log(hashedPassword)
         const user = new User({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
-            email: req.body.email,
+            email: req.body.email.toLowerCase(),
             password: hashedPassword,
         })
         
-        console.log(user)
 
         user.save()
         .then(result =>{
@@ -94,6 +156,43 @@ router.post("/", async (req, res, next) =>{
 })
 
 //*****************************************PATCH requests*******************************************************
+
+router.patch('/save-cart', async (req, res, next) =>{
+    console.log(req.body)
+
+    try{
+        await User.findByIdAndUpdate({_id: req.body.userId}, {$set: {cart: req.body.cart}})
+        res.status(200).json("ok")
+
+    } catch(error) {
+        res.status(500).json(error)
+    }
+})
+
+
+router.patch('/save-favorites', async (req, res, next) =>{
+    console.log(req.body)
+
+    try{
+        await User.findByIdAndUpdate({_id: req.body.userId}, {$set: {favorites: req.body.favorites}})
+        res.status(200).json("ok")
+
+    } catch(error) {
+        res.status(500).json(error)
+    }
+})
+
+router.patch('/save-address', async (req, res, next) =>{
+    console.log(req.body)
+
+    try{
+        await User.findByIdAndUpdate({_id: req.body.userId}, {$set: {address: req.body.address}})
+        res.status(200).json("ok")
+
+    } catch(error) {
+        res.status(500).json(error)
+    }
+})
 
 router.patch("/:userId", (req, res, next) => {
     const id = req.params.userId;
@@ -124,8 +223,8 @@ router.patch("/:userId", (req, res, next) => {
 
 //*****************************************DELETE requests*******************************************************
 
-router.delete("/:userId", (req, res, next) => {
-    const id = req.params.userId;
+router.delete("/delete-user", (req, res, next) => {
+    const id = req.body.userId;
     User.deleteOne({ _id: id })
       .exec()
       .then(result => {
